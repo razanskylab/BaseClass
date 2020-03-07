7506e5756fccc33fd2fa53a19017f9e9a39949c5
Johannes Rebling <johannesrebling@gmail.com>
2020-03-07 15:02:02 +0100

    return done str

--- @BaseClass/Done.m
+++ @BaseClass/Done.m
@@ -2,11 +2,11 @@
 % just look at the code, really...
 % Johannes Rebling, (johannesrebling@gmail.com), 2018
 
-function Done(BC,ticMarker)
+function [doneStr] = Done(BC,ticMarker)
   if nargin == 2
-    BC.VPrintF('done (%3.2f s).\n',toc(ticMarker));
+    doneStr = sprintf('done (%3.2f s).\n',toc(ticMarker));
   else
-    BC.VPrintF('done (%3.2f s).\n',toc());
+    doneStr = sprintf('done (%3.2f s).\n',toc());
   end
-
+  BC.VPrintF(doneStr);
 end
